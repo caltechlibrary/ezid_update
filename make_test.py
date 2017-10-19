96c7fad21b42ab356108b06755226b12f75e9e31
Tom Morrell <tmorrell@caltech.edu>
2017-10-19 14:59:13 -0700

    Fix up test

--- make_test.py
+++ make_test.py
@@ -20,12 +20,13 @@ assert schema40.validate(metadata)
 xml = schema40.tostring(metadata)
 
 resp = ez.mint('doi:10.5072/FK2', {'datacite':xml})
+print(resp)
 #resp = ez.mint('doi:10.5072/FK2',{'datacite.title': 'test title',
 #        'datacite.creator': 'mer','datacite.publisher':
 #        'CD','datacite.publicationyear':'2017','datacite.resourcetype':'Dataset'})
 #print(resp)
-doi = resp.split('|')[0].split()[0].decode('utf-8')
-print doi
+doi = resp.split('|')[0].split()[0]#.decode('utf-8')
+print(doi)
 
 #print('doi:10.5072/FK2K64J47Z')
 resp = ez.update(doi,{'_target':"http://www.caltech.edu"})
